1b1974a5d3f64c80b5bda471236b43c9edf261f4
rere950303 <yhwjjang1995@naver.com>
2021-02-22 00:48:01 +0900

    feat: PhoneBook project step2

---
+++ PhoneBook project(step2)/phoneFunc.h
@@ -0,0 +1,20 @@
+//
+//  phoneFunc.h
+//  PhoneBook prject 2
+//
+//  Created by hyungwook on 2021/02/21.
+//
+
+#ifndef __PHONEFUNC_H__
+#define __PHONEFUNC_H__
+
+#include "phoneData.h"
+
+void InputPhoneData(void);
+void ShowAllData(void);
+void SearchPhoneData(void);
+void DeletePhoneData(void);
+
+
+
+#endif
