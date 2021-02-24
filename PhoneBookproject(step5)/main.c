7bd60a505c515cb84e2eb57bfc6dee0de011cf33
rere950303 <yhwjjang1995@naver.com>
2021-02-24 22:44:06 +0900

    feat: PhoneBook project step5

---
+++ PhoneBookproject(step5)/main.c
@@ -0,0 +1,53 @@
+//
+//  main.c
+//  PhoneBook prject 3
+//
+//  Created by hyungwook on 2021/02/21.
+//
+
+#include "common.h"
+#include "screenOut.h"
+#include "phoneFunc.h"
+
+enum { INPUT = 1, SHOWALL, SEARCH, DELETE, QUIT };
+
+int main(void)
+{
+    LoadDataFromFile();
+    int inputMenu = 0;
+
+    while (1)
+    {
+        ShowMenu();
+        fputs("선택하세요 : ", stdout);
+        scanf("%d", &inputMenu);
+        fflush(stdin);
+
+        switch (inputMenu)
+        {
+        case INPUT:
+            InputPhoneData();
+            break;
+
+        case SHOWALL:
+            ShowAllData();
+            break;
+    
+        case SEARCH:
+            SearchPhoneData();
+            break;
+                
+        case DELETE:
+            DeletePhoneData();
+            break;
+        }
+
+        if (inputMenu == QUIT)
+        {
+            StoreDataToFile();
+            puts("이용해 주셔서 고마워요~");
+            break;
+        }
+    }
+    return 0;
+}
